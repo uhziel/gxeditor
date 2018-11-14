c91d1074a1ef1dd5b1751366f9cd8263763184c0
uhziel <uhziel@gmail.com>
2018-11-14 21:51:03 +0800

    整理了下菜单，把能中文的加上。编辑菜单还不好搞，后续加上。

--- main.js
+++ main.js
@@ -7,6 +7,49 @@ const { Menu, ipcMain } = require('electron');
 let mainWindow
 let safeExit = false;
 
+function genMenu() {
+  var appMenuTemplate = [
+    {
+      label: '文件',
+      submenu: [
+        {
+          label: "打开",
+          click() {
+            mainWindow.webContents.send('action', 'open');
+          },
+          accelerator: 'CmdOrCtrl+O'
+        },
+        {
+          label: "保存",
+          click() {
+            mainWindow.webContents.send('action', 'save');
+          },
+          accelerator: 'CmdOrCtrl+S'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: "退出",
+          role: 'quit'
+        }
+      ]
+    },
+    {
+      label: '帮助',
+      role: 'help',
+      submenu: [
+        {
+          label: '主页',
+          click() { require('electron').shell.openExternal('http://www.baidu.com'); }
+        }
+      ]
+    }
+  ];
+  const appMenu = Menu.buildFromTemplate(appMenuTemplate);
+  Menu.setApplicationMenu(appMenu);
+}
+
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({ width: 800, height: 600 })
@@ -32,52 +75,7 @@ function createWindow() {
     }
   });
 
-  // 创建菜单
-  var appMenuTemplate = [
-    {
-      label: 'File',
-      submenu: [
-        {
-          label: "新建",
-          click() {
-            mainWindow.webContents.send('action', 'new');
-          },
-          accelerator: 'CmdOrCtrl+N'
-        },
-        {
-          label: "打开",
-          click() {
-            mainWindow.webContents.send('action', 'open'); //点击后向主页渲染进程发送“打开文件”的命令
-          },
-          accelerator: 'CmdOrCtrl+O' //快捷键：Ctrl+O
-        },
-        {
-          label: "保存",
-          click() {
-            mainWindow.webContents.send('action', 'save'); //点击后向主页渲染进程发送“保存文件”的命令
-          },
-          accelerator: 'CmdOrCtrl+S' //快捷键：Ctrl+S
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'quit'
-        }
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Home Page',
-          click() { require('electron').shell.openExternal('http://www.baidu.com'); }
-        }
-      ]
-    }
-  ];
-  const appMenu = Menu.buildFromTemplate(appMenuTemplate);
-  Menu.setApplicationMenu(appMenu);
+  genMenu();
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
