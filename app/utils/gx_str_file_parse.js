5ebbd2655805cb61b412afd4a74f85b1a07eacba
uhziel <uhziel@gmail.com>
2019-05-25 16:59:42 +0800

    如果解析CSV失败，不要抛出异常给外部影响外部的逻辑，只要返回空即可

--- app/utils/gx_str_file_parse.js
+++ app/utils/gx_str_file_parse.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const iconv = require("iconv-lite");
 const csvParse = require("csv-parse/lib/sync");
+const logger = require("electron-log");
 
 function parse(filePath, fileEncoding) {
     let strings = {};
@@ -20,13 +21,19 @@ function parse(filePath, fileEncoding) {
 
     const buffer = fs.readFileSync(filePath);
     const content = iconv.decode(buffer, fileEncoding);
-    const csvAarry = csvParse(content, {
-        relax: true,
-        skip_empty_lines: true
-    });
-    csvAarry.forEach(element => {
-        strings[element[0]] = element[1];
-    });
+    try {
+        const csvAarry = csvParse(content, {
+            relax: true,
+            skip_empty_lines: true
+        });
+        csvAarry.forEach(element => {
+            strings[element[0]] = element[1];
+        });
+    }
+    catch (error) {
+        logger.error(error);
+        return strings;
+    }
 
     return strings;
 }
